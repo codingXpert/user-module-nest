e75b90ae94dfdbfc9a88390de7038c08d168a0bc
Vivek Raj <vivek.raj@brainvire.com>
2023-01-10 17:32:18 +0530

    adding userId in return

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -12,8 +12,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       secretOrKey: jwtConstants.secret,
     });
   }
-
-  async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username };
+ 
+    async validate(payload: any) {
+    return { userId: payload.userId, username: payload.username };
   }
 }
